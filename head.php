59dfd91b3f7895ff880a949a5456f9b41dd914c7
flo-afk <66073238+flo-afk@users.noreply.github.com>
2020-08-28 14:14:30 +0200

    Add files via upload

--- head.php
+++ head.php
@@ -4,6 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> SimpleCare </title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="image/x-icon" href="Favicon.png">
 </head>     
